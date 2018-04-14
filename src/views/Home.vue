69b1741464eda2076f44dd209f68bcc08f8799ba
Mir Ayman Ali <alimirayman@outlook.com>
2018-04-14 09:20:16 +0600

    basic setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {
+  loadComponent
+} from '../helpers/importer'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld: loadComponent('HelloWorld')
   }
 }
 </script>
